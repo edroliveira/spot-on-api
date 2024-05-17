2684de01e6246fecf91d1d54c87e8b9767238e0a
Ednaldo Romer de Oliveira Júnior <ednaldojunior2001@gmail.com>
2024-05-17 19:21:18 -0300

    first commit

---
+++ src/test/java/com/sol/spoton/SpotonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sol.spoton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
